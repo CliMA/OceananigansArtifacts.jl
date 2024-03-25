b787d757e4a5f4f6c611f7c8bf00b9bfd20a718a
Sid <siddhartha.bishnu@gmail.com>
2024-03-25 18:55:24 -0400

    Self-sufficient MITgcm binary grid files

--- cubed_sphere_grids/cs32_with_4_halos/cubed_sphere_32_grid_with_4_halos.jl
+++ cubed_sphere_grids/cs32_with_4_halos/cubed_sphere_32_grid_with_4_halos.jl
@@ -31,31 +31,37 @@ Hx, Hy = 4, 4
  λᶠᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
  φᶜᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
  φᶠᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
+Δxᶜᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Δxᶠᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Δxᶜᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
+Δxᶠᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
+Δyᶜᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Δyᶠᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Δyᶜᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
+Δyᶠᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Azᶜᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Azᶠᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Azᶜᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Azᶠᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 
 for region in 1:6
-     λᶜᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/xC.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-     λᶠᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/xG.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-     φᶜᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/yC.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-     φᶠᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/yG.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+     λᶜᶜᵃ_parent[:, :, region] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/xC.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+     λᶠᶠᵃ_parent[:, :, region] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/xG.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+     φᶜᶜᵃ_parent[:, :, region] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/yC.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+     φᶠᶠᵃ_parent[:, :, region] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/yG.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δxᶜᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dxF.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
     Δxᶠᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dXc.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
     Δxᶜᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dXg.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δxᶠᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dxV.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δyᶜᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dyF.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
     Δyᶠᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dYg.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
     Δyᶜᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dYc.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δyᶠᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dyU.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
     Azᶜᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAc.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
     Azᶠᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAw.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
     Azᶜᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAs.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
     Azᶠᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAz.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
 end
-
-cs32_grid_file_missing_metrics = jldopen("cubed_sphere_32_grid_with_4_halos_missing_metrics.jld2")
 
 jldopen("cubed_sphere_32_grid_with_4_halos.jld2", "w") do file
     for region in 1:6
@@ -63,20 +69,17 @@ jldopen("cubed_sphere_32_grid_with_4_halos.jld2", "w") do file
         file["face" * string(region) * "/λᶠᶠᵃ" ] =  λᶠᶠᵃ_parent[:, :, region]
         file["face" * string(region) * "/φᶜᶜᵃ" ] =  φᶜᶜᵃ_parent[:, :, region]
         file["face" * string(region) * "/φᶠᶠᵃ" ] =  φᶠᶠᵃ_parent[:, :, region]
+        file["face" * string(region) * "/Δxᶜᶜᵃ"] = Δxᶜᶜᵃ_parent[:, :, region]
         file["face" * string(region) * "/Δxᶠᶜᵃ"] = Δxᶠᶜᵃ_parent[:, :, region]
         file["face" * string(region) * "/Δxᶜᶠᵃ"] = Δxᶜᶠᵃ_parent[:, :, region]
+        file["face" * string(region) * "/Δxᶠᶠᵃ"] = Δxᶠᶠᵃ_parent[:, :, region]
+        file["face" * string(region) * "/Δyᶜᶜᵃ"] = Δyᶜᶜᵃ_parent[:, :, region]
         file["face" * string(region) * "/Δyᶠᶜᵃ"] = Δyᶠᶜᵃ_parent[:, :, region]
         file["face" * string(region) * "/Δyᶜᶠᵃ"] = Δyᶜᶠᵃ_parent[:, :, region]
+        file["face" * string(region) * "/Δyᶠᶠᵃ"] = Δyᶠᶠᵃ_parent[:, :, region]
         file["face" * string(region) * "/Azᶜᶜᵃ"] = Azᶜᶜᵃ_parent[:, :, region]
         file["face" * string(region) * "/Azᶠᶜᵃ"] = Azᶠᶜᵃ_parent[:, :, region]
         file["face" * string(region) * "/Azᶜᶠᵃ"] = Azᶜᶠᵃ_parent[:, :, region]
         file["face" * string(region) * "/Azᶠᶠᵃ"] = Azᶠᶠᵃ_parent[:, :, region]
-        # Fill the following metrics with their Oceananigans counterparts for now.
-        file["face" * string(region) * "/Δxᶜᶜᵃ"] = cs32_grid_file_missing_metrics["face" * string(region) * "/Δxᶜᶜᵃ"]
-        file["face" * string(region) * "/Δyᶜᶜᵃ"] = cs32_grid_file_missing_metrics["face" * string(region) * "/Δyᶜᶜᵃ"]
-        file["face" * string(region) * "/Δxᶠᶠᵃ"] = cs32_grid_file_missing_metrics["face" * string(region) * "/Δxᶠᶠᵃ"]
-        file["face" * string(region) * "/Δyᶠᶠᵃ"] = cs32_grid_file_missing_metrics["face" * string(region) * "/Δyᶠᶠᵃ"]
     end
 end
-
-close(cs32_grid_file_missing_metrics)
