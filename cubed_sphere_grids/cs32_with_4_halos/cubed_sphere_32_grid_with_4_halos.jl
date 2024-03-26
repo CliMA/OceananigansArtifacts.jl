ca2b81c9e6965075756827a93a2f859c42180058
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-03-26 11:18:20 +0200

    bit cleanup

--- cubed_sphere_grids/cs32_with_4_halos/cubed_sphere_32_grid_with_4_halos.jl
+++ cubed_sphere_grids/cs32_with_4_halos/cubed_sphere_32_grid_with_4_halos.jl
@@ -1,14 +1,14 @@
 using JLD2
 
-function read_big_endian_coordinates(filename, nInterior = 32, Nhalo = 1)
+function read_big_endian_coordinates(filename, Ninterior = 32, Nhalo = 1)
     # Open the file in binary read mode
     open(filename, "r") do io
         # Calculate the number of Float64 values in the file
         n = filesize(io) ÷ sizeof(Float64)
 
-        # Ensure n = (nInterior + 2 * Nhalo) * (nInterior + 2 * Nhalo)
-        if n != (nInterior + 2 * Nhalo) * (nInterior + 2 * Nhalo)
-            error("File size does not match the expected size for one (nInterior + 2 * Nhalo) x (nInterior + 2 * Nhalo) field")
+        # Ensure n = (Ninterior + 2 * Nhalo) * (Ninterior + 2 * Nhalo)
+        if n != (Ninterior + 2 * Nhalo) * (Ninterior + 2 * Nhalo)
+            error("File size does not match the expected size for an (Ninterior + 2 * Nhalo) x (Ninterior + 2 * Nhalo) field")
         end
 
         # Initialize an array to hold the data
@@ -18,11 +18,17 @@ function read_big_endian_coordinates(filename, nInterior = 32, Nhalo = 1)
         read!(io, data)
 
         # Convert from big-endian to native endianness
-        native_data = reshape(bswap.(data), (nInterior + 2 * Nhalo), (nInterior + 2 * Nhalo))
+        native_data = reshape(bswap.(data), (Ninterior + 2 * Nhalo), (Ninterior + 2 * Nhalo))
 
         return native_data
     end
 end
+
+#####
+##### Convert .bin files to .jld2
+#####
+
+cubed_sphere_filepath = "cubed_sphere_32_grid_with_4_halos.jld2"
 
 Nx, Ny, Nz = 32, 32, 1
 Hx, Hy = 4, 4
@@ -44,42 +50,44 @@ Azᶠᶜᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Azᶜᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 Azᶠᶠᵃ_parent = zeros(Nx+2Hx, Ny+2Hy, 6)
 
-for region in 1:6
-     λᶜᶜᵃ_parent[:, :, region] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/xC.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-     λᶠᶠᵃ_parent[:, :, region] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/xG.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-     φᶜᶜᵃ_parent[:, :, region] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/yC.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-     φᶠᶠᵃ_parent[:, :, region] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/yG.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Δxᶜᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dxF.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Δxᶠᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dXc.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Δxᶜᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dXg.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Δxᶠᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dxV.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Δyᶜᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dyF.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Δyᶠᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dYg.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Δyᶜᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dYc.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Δyᶠᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dyU.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Azᶜᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAc.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Azᶠᶜᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAw.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Azᶜᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAs.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
-    Azᶠᶠᵃ_parent[:, :, region] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAz.00$(region).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+for panel in 1:6
+     λᶜᶜᵃ_parent[:, :, panel] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/xC.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+     λᶠᶠᵃ_parent[:, :, panel] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/xG.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+     φᶜᶜᵃ_parent[:, :, panel] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/yC.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+     φᶠᶠᵃ_parent[:, :, panel] =  read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/yG.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δxᶜᶜᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dxF.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δxᶠᶜᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dXc.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δxᶜᶠᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dXg.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δxᶠᶠᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dxV.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δyᶜᶜᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dyF.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δyᶠᶜᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dYg.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δyᶜᶠᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dYc.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Δyᶠᶠᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/dyU.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Azᶜᶜᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAc.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Azᶠᶜᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAw.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Azᶜᶠᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAs.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
+    Azᶠᶠᵃ_parent[:, :, panel] = read_big_endian_coordinates("cubed_sphere_32_grid_with_4_halos/rAz.00$(panel).001.data", 32, 4)[1+4-Hx:end-4+Hx, 1+4-Hy:end-4+Hy]
 end
 
-jldopen("cubed_sphere_32_grid_with_4_halos.jld2", "w") do file
-    for region in 1:6
-        file["face" * string(region) * "/λᶜᶜᵃ" ] =  λᶜᶜᵃ_parent[:, :, region]
-        file["face" * string(region) * "/λᶠᶠᵃ" ] =  λᶠᶠᵃ_parent[:, :, region]
-        file["face" * string(region) * "/φᶜᶜᵃ" ] =  φᶜᶜᵃ_parent[:, :, region]
-        file["face" * string(region) * "/φᶠᶠᵃ" ] =  φᶠᶠᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Δxᶜᶜᵃ"] = Δxᶜᶜᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Δxᶠᶜᵃ"] = Δxᶠᶜᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Δxᶜᶠᵃ"] = Δxᶜᶠᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Δxᶠᶠᵃ"] = Δxᶠᶠᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Δyᶜᶜᵃ"] = Δyᶜᶜᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Δyᶠᶜᵃ"] = Δyᶠᶜᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Δyᶜᶠᵃ"] = Δyᶜᶠᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Δyᶠᶠᵃ"] = Δyᶠᶠᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Azᶜᶜᵃ"] = Azᶜᶜᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Azᶠᶜᵃ"] = Azᶠᶜᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Azᶜᶠᵃ"] = Azᶜᶠᵃ_parent[:, :, region]
-        file["face" * string(region) * "/Azᶠᶠᵃ"] = Azᶠᶠᵃ_parent[:, :, region]
+jldopen(cubed_sphere_filepath, "w") do file
+    for panel in 1:6
+        file["panel" * string(panel) * "/λᶜᶜᵃ" ] =  λᶜᶜᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/λᶠᶠᵃ" ] =  λᶠᶠᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/φᶜᶜᵃ" ] =  φᶜᶜᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/φᶠᶠᵃ" ] =  φᶠᶠᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Δxᶜᶜᵃ"] = Δxᶜᶜᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Δxᶠᶜᵃ"] = Δxᶠᶜᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Δxᶜᶠᵃ"] = Δxᶜᶠᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Δxᶠᶠᵃ"] = Δxᶠᶠᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Δyᶜᶜᵃ"] = Δyᶜᶜᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Δyᶠᶜᵃ"] = Δyᶠᶜᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Δyᶜᶠᵃ"] = Δyᶜᶠᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Δyᶠᶠᵃ"] = Δyᶠᶠᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Azᶜᶜᵃ"] = Azᶜᶜᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Azᶠᶜᵃ"] = Azᶠᶜᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Azᶜᶠᵃ"] = Azᶜᶠᵃ_parent[:, :, panel]
+        file["panel" * string(panel) * "/Azᶠᶠᵃ"] = Azᶠᶠᵃ_parent[:, :, panel]
     end
 end
+
+@info "Generated $cubed_sphere_filepath"
