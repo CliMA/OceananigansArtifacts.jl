6c30a3241836246556ffc48ca5556b44775bfd41
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-03-27 07:15:13 +0200

    add info for binary files

--- cubed_sphere_grids/cs32_with_4_halos/cubed_sphere_32_grid_with_4_halos.jl
+++ cubed_sphere_grids/cs32_with_4_halos/cubed_sphere_32_grid_with_4_halos.jl
@@ -1,3 +1,11 @@
+# by J.-M. C.
+# 
+# The grid-metric binary files correspond to MITgcm standard cs32 grid
+# (grid_cs32.face00[1-6].bin, from verification experiment tutorial_held_suarez_cs/input)
+# and halo-regions were added by running exp. advect_cs with minor changes
+# to model/src/ini_curvilinear_grid.F (un-commenting lines 422-426 and adding
+# missing WRITE_FULLARRAY_RS calls).
+
 using JLD2
 
 function read_big_endian_coordinates(filename, Ninterior = 32, Nhalo = 1)
